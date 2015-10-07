0e59a0e60bd503d415d19d7f2f09cf0ffa6df2ea
Molinet, Fabien <fabien.molinet@hager.fr>
2015-10-07 14:53:50 +0200

    Bait & switch pattern for transformations

---
+++ FFImageLoading.BaitAndSwitch/Transformations/GrayscaleTransformation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FFImageLoading.Work;
+
+namespace FFImageLoading.Transformations
+{
+	public class GrayscaleTransformation: ITransformation
+	{
+		private const string DoNotReference = "You are referencing the Portable version in your App - you need to reference the platform (iOS/Android) version";
+
+		public IBitmap Transform(IBitmap source)
+		{
+			throw new Exception(DoNotReference);
+		}
+		public string Key
+		{
+			get
+			{
+				throw new Exception(DoNotReference);
+			}
+		}
+	}
+}
+
