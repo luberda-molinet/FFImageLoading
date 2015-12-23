4790c06a8ce2aff6330a16e47f0b107afe37ba55
Andrei Pachtarou <andrei.pachtarou@gmail.com>
2015-12-23 15:22:40 +0300

    ios: add ios sample wit UIKit ui

---
+++ samples/Simple.iOS.Sample/ImageDetailsViewController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using UIKit;
+
+using FFImageLoading;
+using FFImageLoading.Work;
+using FFImageLoading.Transformations;
+
+namespace Simple.iOS.Sample
+{
+    public partial class ImageDetailsViewController : UIViewController
+    {
+        public const string SegueImageDetails = "ImageDetails-segue";
+
+        public string imageURL = string.Empty;
+
+        public ImageDetailsViewController(IntPtr handle)
+            : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            transformation = 0;
+            LoadImage();
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            imageURL = string.Empty;
+        }
+
+        int transformation = 0;
+
+        protected void LoadImage()
+        {
+            var taskImage = ImageService.LoadUrl(imageURL)
+                                        .ErrorPlaceholder("error@2x.png", ImageSource.ApplicationBundle)
+                                        .LoadingPlaceholder("placeholder.png", ImageSource.CompiledResource);
+            if(transformation==0)
+            {                
+                taskImage.Into(imageView);
+                transformation++;
+            }
+            else if(transformation==1)
+            {
+                taskImage.Transform(new CircleTransformation())                
+                         .Into(imageView);
+                transformation++;
+            }
+            else if(transformation==2)
+            {
+                taskImage.Transform(new RoundedTransformation(10))                
+                         .Into(imageView);
+                transformation = 0;
+            }
+        }
+
+        partial void TapTransformation (Foundation.NSObject sender)
+        {
+            LoadImage();
+        }
+
+    }
+}
+
+
