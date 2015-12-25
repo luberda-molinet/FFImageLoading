ced9ed922e23dcf82014ca0e9367caeb1384210a
Daniel Luberda <daniel.luberda@gmail.com>
2015-12-25 14:42:16 +0100

    Windows: Added missing namespace

--- source/FFImageLoading.Windows/TaskParameterExtensions.cs
+++ source/FFImageLoading.Windows/TaskParameterExtensions.cs
@@ -6,6 +6,7 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml;
+using FFImageLoading.Cache;
 
 namespace FFImageLoading
 {
