e165d7102c63e32f9da4bd477597a2f1f41a6cc5
Daniel Luberda <daniel.luberda@gmail.com>
2016-01-15 22:44:59 +0100

    Windows: Fixed UWP GetForCurrentView must be called on a UI thread

--- source/FFImageLoading.Windows/Extensions/UnitsExtensions.cs
+++ source/FFImageLoading.Windows/Extensions/UnitsExtensions.cs
@@ -1,14 +1,36 @@
-﻿using System;
+﻿using FFImageLoading.Helpers;
+using System;
 using Windows.Graphics.Display;
 
 namespace FFImageLoading.Extensions
 {
     public static class UnitsExtensions
     {
+        static UnitsExtensions()
+        {
+            InitResolutionScale();
+        }
+
+        static double resolutionScale = -1d;
+
+        static async void InitResolutionScale()
+        {
+            await MainThreadDispatcher.Instance.PostAsync(() =>
+            {
+                resolutionScale = (double)DisplayInformation.GetForCurrentView().ResolutionScale / 100.0d;
+            });
+        }
+
+        static void WaitForResolutionScaleInit()
+        {
+            while (resolutionScale == -1d) { /* wait */ }
+        }
+
         public static int PointsToPixels(this double points)
         {
-            var scale = (double)DisplayInformation.GetForCurrentView().ResolutionScale / 100.0f; 
-            return (int)Math.Floor(points * scale);
+            WaitForResolutionScaleInit();
+
+            return (int)Math.Floor(points * resolutionScale);
         }
 
         public static int PixelsToPoints(this double px)
@@ -16,8 +38,9 @@ namespace FFImageLoading.Extensions
             if (px == 0d)
                 return 0;
 
-            var scale = (double)DisplayInformation.GetForCurrentView().ResolutionScale / 100.0f;
-            return (int)Math.Floor(px / scale);
+            WaitForResolutionScaleInit();
+
+            return (int)Math.Floor(px / resolutionScale);
         }
 
         public static int PointsToPixels(this int points)
