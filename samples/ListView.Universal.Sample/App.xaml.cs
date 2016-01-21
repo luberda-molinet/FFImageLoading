1077178fbd307772d43b5f8994157808ced077b7
Sebastian Frysztak <sebfry@gmail.com>
2016-01-21 21:54:29 +0100

    Update ListView sample.

--- samples/ListView.Universal.Sample/App.xaml.cs
+++ samples/ListView.Universal.Sample/App.xaml.cs
@@ -43,7 +43,7 @@ namespace ListView.Universal.Sample
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                //this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
 
--- samples/ListView.Universal.Sample/MainPage.xaml.cs
+++ samples/ListView.Universal.Sample/MainPage.xaml.cs
@@ -32,6 +32,7 @@ namespace ListView.Universal.Sample
 
         private DispatcherTimer _timer = null;
         private int _urlIdx = 0;
+        private Random _random = new Random();
 
         public MainPage()
         {
@@ -42,16 +43,20 @@ namespace ListView.Universal.Sample
                 URLs.Add($"http://loremflickr.com/400/400?filename=simple.jpg?random={i}");
 
             _timer = new DispatcherTimer();
-            _timer.Interval = new TimeSpan(0, 0, 0, 0, 500); // 250 ms
+            _timer.Interval = new TimeSpan(0, 0, 10); // 2 seconds
             _timer.Tick += _timer_Tick;
         }
 
         private void _timer_Tick(object sender, object e)
         {
-            if (_urlIdx < URLs.Count)
+            var idx = _random.Next(0, URLs.Count - 1);
+            var rnd = _random.Next();
+            URLs[0] = $"http://loremflickr.com/400/400?filename=simple.jpg?random={rnd}";
+
+            /* if (_urlIdx < URLs.Count)
                 ListView.ScrollIntoView(URLs[_urlIdx++]);
             else
-                _timer.Stop();
+                _timer.Stop();*/
         }
 
         private void ListView_Loaded(object sender, RoutedEventArgs e)
