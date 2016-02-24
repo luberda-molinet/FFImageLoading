e4e5870074f48df0ce44d9cc1ed1db2fe2f8a1a6
Molinet, Fabien <fabien.molinet@hager.fr>
2016-02-24 08:19:25 +0100

    Remove legacy code #138

--- source/FFImageLoading.Droid/Work/ImageLoaderTask.cs
+++ source/FFImageLoading.Droid/Work/ImageLoaderTask.cs
@@ -657,12 +657,6 @@ namespace FFImageLoading.Work
 
 							imageView.SetImageDrawable(value);
 
-							if (Utils.HasJellyBean() && imageView.AdjustViewBounds)
-							{
-								imageView.LayoutParameters.Height = value.IntrinsicHeight;
-								imageView.LayoutParameters.Width = value.IntrinsicWidth;
-							}
-
 							Completed = true;
 
 							Parameters?.OnSuccess(cacheEntry.Item2, LoadingResult.MemoryCache);
@@ -813,17 +807,6 @@ namespace FFImageLoading.Work
 			return this == task
 				? imageView
 				: null;
-		}
-
-		private void SetImageDrawable(ImageView imageView, Drawable drawable)
-		{
-			if (Utils.HasJellyBean() && imageView.AdjustViewBounds)
-			{
-				imageView.LayoutParameters.Height = drawable.IntrinsicHeight;
-				imageView.LayoutParameters.Width = drawable.IntrinsicWidth;
-			}
-
-			imageView.SetImageDrawable(drawable);
 		}
 	}
 }
