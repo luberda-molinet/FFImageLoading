cee26478b87402154e4a2dc0657bda51b90adc65
Daniel Luberda <daniel.luberda@gmail.com>
2016-02-25 15:17:51 +0100

    Some build automation enhancements

---
+++ MakefileWindows.bat
@@ -0,0 +1,12 @@
+"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Common/FFImageLoading.csproj /nologo /p:Configuration=Release /consoleloggerparameters:ErrorsOnly /verbosity:minimal
+
+"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Windows/FFImageLoading.Windows.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
+"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.WinSL/FFImageLoading-contrib.WinSL.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
+
+"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Transformations.Windows/FFImageLoading.Transformations.Windows.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
+"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Transformations.WinSL/FFImageLoading.Transformations.WinSL.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
+
+"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Forms.WinRT/FFImageLoading.Forms.WinRT.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
+"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Forms.WinSL/FFImageLoading.Forms.WinSL.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
+
+pause
