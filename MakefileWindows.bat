06fe9e0077a77bde1683be193a3f24abca88b636
Daniel Luberda <daniel.luberda@gmail.com>
2016-03-02 14:37:39 +0100

    Some minor project changes

--- MakefileWindows.bat
+++ MakefileWindows.bat
@@ -1,12 +1,13 @@
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Common/FFImageLoading.csproj /nologo /p:Configuration=Release /consoleloggerparameters:ErrorsOnly /verbosity:minimal
 
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Windows/FFImageLoading.Windows.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
-"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.WinSL/FFImageLoading-contrib.WinSL.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
+"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.WinSL/FFImageLoading.WinSL.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
 
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Transformations.Windows/FFImageLoading.Transformations.Windows.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Transformations.WinSL/FFImageLoading.Transformations.WinSL.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
 
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Forms.WinRT/FFImageLoading.Forms.WinRT.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
+"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Forms.WinUWP/FFImageLoading.Forms.WinUWP.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" source/FFImageLoading.Forms.WinSL/FFImageLoading.Forms.WinSL.csproj /nologo /p:Configuration="Windows Release" /consoleloggerparameters:ErrorsOnly /verbosity:minimal
 
 pause
