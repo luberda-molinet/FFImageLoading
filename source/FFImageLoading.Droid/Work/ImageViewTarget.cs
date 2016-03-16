1a6e1b463ae7f00256a35538a2c581786deae164
Molinet, Fabien <fabien.molinet@hager.fr>
2016-03-16 23:13:33 +0100

    ImageLoaderTask can handle different kind of Target. Useful for preloading for example.

---
+++ source/FFImageLoading.Droid/Work/ImageViewTarget.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Android.Graphics.Drawables;
+using Android.Widget;
+using System.Threading.Tasks;
+using FFImageLoading.Extensions;
+
+namespace FFImageLoading.Work
+{
+	public class ImageViewTarget: Target<BitmapDrawable, ImageLoaderTask>
+	{
+		private readonly WeakReference<ImageView> _controlWeakReference;
+
+		public ImageViewTarget(ImageView control)
+		{
+			_controlWeakReference = new WeakReference<ImageView>(control);
+		}
+
+		public override bool IsValid
+		{
+			get
+			{
+				return Control != null;
+			}
+		}
+
+		public override bool IsTaskValid(ImageLoaderTask task)
+		{
+			var control = Control;
+			if (control == null)
+				return false;
+
+			var controlTask = control.GetImageLoaderTask();
+			return controlTask == task;
+		}
+
+		public override void Set(ImageLoaderTask task, BitmapDrawable image, bool isLocalOrFromCache, bool isLoadingPlaceholder)
+		{
+			if (task.IsCancelled)
+				return;
+			
+			var control = Control;
+			if (control == null)
+				return;
+
+			control.SetImageDrawable(image);
+		}
+
+		public override bool UsesSameNativeControl(ImageLoaderTask task)
+		{
+			var otherTarget = task._target as ImageViewTarget;
+			if (otherTarget == null)
+				return false;
+
+			var control = Control;
+			var otherControl = otherTarget.Control;
+			if (control == null || otherControl == null)
+				return false;
+
+			return control.Handle == otherControl.Handle;
+		}
+
+		private ImageView Control
+		{
+			get
+			{
+				ImageView control;
+				if (!_controlWeakReference.TryGetTarget(out control))
+					return null;
+
+				if (control == null || control.Handle == IntPtr.Zero)
+					return null;
+
+				return control;
+			}
+		}
+	}
+}
+
---
+++ source/FFImageLoading.Touch/Work/UIImageViewTarget.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using FFImageLoading.Extensions;
+using UIKit;
+
+namespace FFImageLoading.Work
+{
+	public class UIImageViewTarget: UIViewTarget<UIImageView>
+	{
+		public UIImageViewTarget(UIImageView control) : base(control)
+		{
+		}
+
+		public override void Set(ImageLoaderTask task, UIImage image, bool isLocalOrFromCache, bool isLoadingPlaceholder)
+		{
+			var control = Control;
+			if (control == null)
+				return;
+
+			var parameters = task.Parameters;
+
+			bool isFadeAnimationEnabled = parameters.FadeAnimationEnabled ?? ImageService.Instance.Config.FadeAnimationEnabled;
+			bool isFadeAnimationEnabledForCached = isFadeAnimationEnabled && (parameters.FadeAnimationForCachedImages ?? ImageService.Instance.Config.FadeAnimationForCachedImages);
+
+			if (!isLoadingPlaceholder && isFadeAnimationEnabled && (!isLocalOrFromCache || (isLocalOrFromCache && isFadeAnimationEnabledForCached)))
+			{
+				// fade animation
+				double fadeDuration = (double)((parameters.FadeAnimationDuration.HasValue ?
+					parameters.FadeAnimationDuration.Value : ImageService.Instance.Config.FadeAnimationDuration)) / 1000;
+
+				UIView.Transition(control, fadeDuration, 
+					UIViewAnimationOptions.TransitionCrossDissolve 
+					| UIViewAnimationOptions.BeginFromCurrentState,
+					() => { control.Image = image; },
+					() => {  });
+			}
+			else
+			{
+				control.Image = image;
+			}
+		}
+	}
+}
+
