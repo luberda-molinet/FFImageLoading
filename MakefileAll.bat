2c1c82faa855b5733bba4fb0b1bd93beafd60a80
Matthew Leibowitz <mattleibow@live.com>
2016-05-20 20:49:56 +0200

    added logging

--- MakefileAll.bat
+++ MakefileAll.bat
@@ -1,9 +1,15 @@
+@echo off
+
 set msbuild="C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe"
 set config=Windows Release
 set platform=AnyCPU
 set buildargs=/nologo /p:Configuration="%config%" /p:Platform="%platform%" /consoleloggerparameters:ErrorsOnly /verbosity:detailed /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
+echo Restoring NuGets...
+
 nuget restore
+
+echo Building FFImageLoading...
 
 %msbuild% source/FFImageLoading.Common/FFImageLoading.csproj %buildargs%
 %msbuild% source/FFImageLoading.BaitAndSwitch/FFImageLoading.BaitAndSwitch.csproj %buildargs%
@@ -12,11 +18,15 @@ nuget restore
 %msbuild% source/FFImageLoading.Touch/FFImageLoading.Touch.csproj %buildargs%
 %msbuild% source/FFImageLoading.Droid/FFImageLoading.Droid.csproj %buildargs%
 
+echo Building FFImageLoading.Transformations...
+
 %msbuild% source/FFImageLoading.Transformations/FFImageLoading.Transformations.csproj %buildargs%
 %msbuild% source/FFImageLoading.Transformations.Windows/FFImageLoading.Transformations.Windows.csproj %buildargs%
 %msbuild% source/FFImageLoading.Transformations.WinSL/FFImageLoading.Transformations.WinSL.csproj %buildargs%
 %msbuild% source/FFImageLoading.Transformations.Touch/FFImageLoading.Transformations.Touch.csproj %buildargs%
 %msbuild% source/FFImageLoading.Transformations.Droid/FFImageLoading.Transformations.Droid.csproj %buildargs%
+
+echo Building FFImageLoading.Forms...
 
 %msbuild% source/FFImageLoading.Forms.WinRT/FFImageLoading.Forms.WinRT.csproj %buildargs%
 %msbuild% source/FFImageLoading.Forms.WinUWP/FFImageLoading.Forms.WinUWP.csproj %buildargs%
@@ -24,6 +34,10 @@ nuget restore
 %msbuild% source/FFImageLoading.Forms.Touch/FFImageLoading.Forms.Touch.csproj %buildargs%
 %msbuild% source/FFImageLoading.Forms.Droid/FFImageLoading.Forms.Droid.csproj %buildargs%
 
+echo Packaging NuGets...
+
 nuget pack source/Xamarin.FFImageLoading.nuspec
 nuget pack source/Xamarin.FFImageLoading.Transformations.nuspec
 nuget pack source/Xamarin.FFImageLoading.Forms.nuspec
+
+echo All done.
