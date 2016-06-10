61773f2ca389cb77d27128d19170f3da83c1e8ba
Molinet, Fabien <fabien.molinet@hager.fr>
2016-06-10 07:48:25 +0200

    Add InvalidateAsync() and Preload() extensions method into Bait & Switch assembly so it can be called from a PCL

---
+++ source/FFImageLoading.BaitAndSwitch/TaskParameterExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using FFImageLoading.Cache;
+using FFImageLoading.Work;
+
+namespace FFImageLoading
+{
+	public static class TaskParameterExtensions
+	{
+		private const string DoNotReference = "You are referencing the Portable version in your App - you need to reference the platform (iOS/Android) version";
+
+		/// <summary>
+		/// Invalidate the image corresponding to given parameters from given caches.
+		/// </summary>
+		/// <param name="parameters">Image parameters.</param>
+		/// <param name="cacheType">Cache type.</param>
+		public static Task InvalidateAsync(this TaskParameter parameters, CacheType cacheType)
+		{
+			throw new Exception(DoNotReference);
+		}
+
+		/// <summary>
+		/// Preload the image request into memory cache/disk cache for future use.
+		/// </summary>
+		/// <param name="parameters">Image parameters.</param>
+		public static void Preload(this TaskParameter parameters)
+		{
+			throw new Exception(DoNotReference);
+		}
+	}
+}
+
