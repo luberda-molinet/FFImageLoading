9ba03ea9530ad1e23614720d705c8c12715398b7
Daniel <daniel@Daniels-MBP.lan>
2016-07-02 05:03:53 +0200

    Xamarin.Forms: ImageSourceConverter readded XF <2.0 compatibilty

--- source/FFImageLoading.Forms/ImageSourceConverter.cs
+++ source/FFImageLoading.Forms/ImageSourceConverter.cs
@@ -1,16 +1,24 @@
 ﻿using System;
+using System.Globalization;
 using Xamarin.Forms;
 
 namespace FFImageLoading.Forms
 {
 	public class ImageSourceConverter : TypeConverter
 	{
-		public override object ConvertFromInvariantString(string value)
+		public override bool CanConvertFrom(Type sourceType)
 		{
-			if (value != null)
+			return sourceType == typeof(string);
+		}
+
+		public override object ConvertFrom(CultureInfo culture, object value)
+		{
+			var text = value as string;
+
+			if (text != null)
 			{
 				Uri uri;
-				return Uri.TryCreate(value, UriKind.Absolute, out uri) && uri.Scheme != "file" ? ImageSource.FromUri(uri) : ImageSource.FromFile(value);
+				return Uri.TryCreate(text, UriKind.Absolute, out uri) && uri.Scheme != "file" ? ImageSource.FromUri(uri) : ImageSource.FromFile(text);
 			}
 
 			throw new InvalidOperationException(string.Format("Cannot convert \"{0}\" into {1}", value, typeof(ImageSource)));
