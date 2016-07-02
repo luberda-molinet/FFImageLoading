d583d65d85132f27a06721d6b1f0e1e62ed94a38
Daniel <daniel@Daniels-MBP.lan>
2016-07-02 04:14:02 +0200

    Xamarin.Forms: Obsolote TypeConverter ImageSourceConverter methods refactorings

--- source/FFImageLoading.Forms/ImageSourceConverter.cs
+++ source/FFImageLoading.Forms/ImageSourceConverter.cs
@@ -1,39 +1,19 @@
 ﻿using System;
 using Xamarin.Forms;
-using System.Globalization;
 
 namespace FFImageLoading.Forms
 {
 	public class ImageSourceConverter : TypeConverter
 	{
-		public override bool CanConvertFrom(Type sourceType)
+		public override object ConvertFromInvariantString(string value)
 		{
-			return sourceType == typeof(string);
-		}
-
-		public override object ConvertFrom(CultureInfo culture, object value)
-		{
-			if (value == null)
+			if (value != null)
 			{
-				return null;
+				Uri uri;
+				return Uri.TryCreate(value, UriKind.Absolute, out uri) && uri.Scheme != "file" ? ImageSource.FromUri(uri) : ImageSource.FromFile(value);
 			}
 
-			string text = value as string;
-			if (text == null)
-			{
-				throw new InvalidOperationException(string.Format("Cannot convert {0} into {1}", new object[] {
-					value,
-					typeof(ImageSource)
-				}));
-			}
-
-			Uri uri;
-			if (!Uri.TryCreate(text, UriKind.Absolute, out uri) || uri.Scheme == "file")
-			{
-				return ImageSource.FromFile(text);
-			}
-
-			return ImageSource.FromUri(uri);
+			throw new InvalidOperationException(string.Format("Cannot convert \"{0}\" into {1}", value, typeof(ImageSource)));
 		}
 	}
 }
