101fa4681ff1cbd5c937d691fda0c77375580998
Daniel Luberda <daniel.luberda@gmail.com>
2016-10-26 13:10:50 +0200

    Disabled Silverlight build

--- MakefileAll.bat
+++ MakefileAll.bat
@@ -18,7 +18,6 @@ echo Building FFImageLoading...
 %msbuild% source/FFImageLoading.Common/FFImageLoading.csproj %buildargs%
 %msbuild% source/FFImageLoading.BaitAndSwitch/FFImageLoading.BaitAndSwitch.csproj %buildargs%
 %msbuild% source/FFImageLoading.Windows/FFImageLoading.Windows.csproj %buildargs%
-%msbuild% source/FFImageLoading.WinSL/FFImageLoading.WinSL.csproj %buildargs%
 %msbuild% source/FFImageLoading.Touch/FFImageLoading.Touch.csproj %buildargs%
 %msbuild% source/FFImageLoading.Droid/FFImageLoading.Droid.csproj %buildargs%
 
@@ -26,7 +25,6 @@ echo Building FFImageLoading.Transformations...
 
 %msbuild% source/FFImageLoading.Transformations/FFImageLoading.Transformations.csproj %buildargs%
 %msbuild% source/FFImageLoading.Transformations.Windows/FFImageLoading.Transformations.Windows.csproj %buildargs%
-%msbuild% source/FFImageLoading.Transformations.WinSL/FFImageLoading.Transformations.WinSL.csproj %buildargs%
 %msbuild% source/FFImageLoading.Transformations.Touch/FFImageLoading.Transformations.Touch.csproj %buildargs%
 %msbuild% source/FFImageLoading.Transformations.Droid/FFImageLoading.Transformations.Droid.csproj %buildargs%
 
@@ -34,7 +32,6 @@ echo Building FFImageLoading.Forms...
 
 %msbuild% source/FFImageLoading.Forms.WinRT/FFImageLoading.Forms.WinRT.csproj %buildargs%
 %msbuild% source/FFImageLoading.Forms.WinUWP/FFImageLoading.Forms.WinUWP.csproj %buildargs%
-%msbuild% source/FFImageLoading.Forms.WinSL/FFImageLoading.Forms.WinSL.csproj %buildargs%
 %msbuild% source/FFImageLoading.Forms.Touch/FFImageLoading.Forms.Touch.csproj %buildargs%
 %msbuild% source/FFImageLoading.Forms.Droid/FFImageLoading.Forms.Droid.csproj %buildargs%
 
