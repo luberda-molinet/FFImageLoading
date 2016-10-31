e4994d22e4040908095259034bb7f8dc51c79b3a
Daniel Luberda <daniel.luberda@gmail.com>
2016-10-31 19:56:09 +0100

    Fixed #350 iOS BundleDataResolver won't load files without extension specified

--- source/FFImageLoading.Touch/DataResolvers/BundleDataResolver.cs
+++ source/FFImageLoading.Touch/DataResolvers/BundleDataResolver.cs
@@ -11,24 +11,25 @@ namespace FFImageLoading.DataResolvers
 {
     public class BundleDataResolver : IDataResolver
 	{
+        readonly string[] fileTypes = { null, "png", "jpg", "jpeg", "webp", "PNG", "JPG", "JPEG", "WEBP"};
+
         public Task<Tuple<Stream, LoadingResult, ImageInformation>> Resolve(string identifier, TaskParameter parameters, CancellationToken token)
         {
-            var fileName = Path.GetFileNameWithoutExtension(identifier);
-            var extension = Path.GetExtension(identifier).TrimStart('.');
-
-            var bundle = NSBundle._AllBundles.FirstOrDefault(bu => !string.IsNullOrEmpty(bu.PathForResource(fileName, extension)));
-
-            if (bundle != null)
+            foreach (var fileType in fileTypes)
             {
-                var url = bundle.GetUrlForResource(fileName, extension);
-                var stream = FileStore.GetInputStream(url.Path, true);
+                var bundle = NSBundle._AllBundles.FirstOrDefault(bu => !string.IsNullOrEmpty(bu.PathForResource(identifier, fileType)));
+                if (bundle != null)
+                {
+                    var path = bundle.PathForResource(identifier, fileType);
+                    var stream = FileStore.GetInputStream(path, true);
 
-                var imageInformation = new ImageInformation();
-                imageInformation.SetPath(identifier);
-                imageInformation.SetFilePath(url.Path);
+                    var imageInformation = new ImageInformation();
+                    imageInformation.SetPath(identifier);
+                    imageInformation.SetFilePath(path);
 
-                return Task.FromResult(new Tuple<Stream, LoadingResult, ImageInformation>(
-                    stream, LoadingResult.CompiledResource, imageInformation));
+                    return Task.FromResult(new Tuple<Stream, LoadingResult, ImageInformation>(
+                        stream, LoadingResult.CompiledResource, imageInformation));
+                }
             }
 
             throw new FileNotFoundException(identifier);
