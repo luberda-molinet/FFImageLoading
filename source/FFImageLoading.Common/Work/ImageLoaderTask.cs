7f1e17831fdd0d223801bb9fe8deb75221d83432
Daniel Luberda <daniel.luberda@gmail.com>
2016-11-09 23:04:06 +0100

    #367 Fixed some IsCompleted and OnSuccess callback issues

--- source/FFImageLoading.Common/Work/ImageLoaderTask.cs
+++ source/FFImageLoading.Common/Work/ImageLoaderTask.cs
@@ -219,6 +219,7 @@ namespace FFImageLoading.Work
                 if (result)
                 {
                     Logger.Debug(string.Format("Image loaded from cache: {0}", Key));
+                    IsCompleted = true;
 
                     if (Configuration.ExecuteCallbacksOnUIThread && (Parameters?.OnSuccess != null || Parameters?.OnFinish != null))
                     {
@@ -233,8 +234,6 @@ namespace FFImageLoading.Work
                         Parameters?.OnSuccess?.Invoke(ImageInformation, LoadingResult.MemoryCache);
                         Parameters?.OnFinish?.Invoke(this);
                     }
-
-                    IsCompleted = true;
                 }
                 else
                 {
@@ -334,13 +333,15 @@ namespace FFImageLoading.Work
 
         public async Task RunAsync()
         {
+            LoadingResult loadingResult = LoadingResult.Failed;
+            bool success = false;
+
             try
             {
                 if (IsCompleted || IsCancelled || ImageService.ExitTasksEarly)
                     throw new OperationCanceledException();
 
                 ThrowIfCancellationRequested();
-                LoadingResult loadingResult = LoadingResult.Failed;
 
                 // LOAD IMAGE
                 if (!(await TryLoadFromMemoryCacheAsync().ConfigureAwait(false)))
@@ -364,17 +365,7 @@ namespace FFImageLoading.Work
                             if (loadingResult == LoadingResult.Internet)
                                 Logger?.Debug(string.Format("DownloadOnly success: {0}", Key));
 
-                            if (Configuration.ExecuteCallbacksOnUIThread && Parameters?.OnSuccess != null)
-                            {
-                                await MainThreadDispatcher.PostAsync(() =>
-                                {
-                                    Parameters?.OnSuccess?.Invoke(ImageInformation, loadingResult);
-                                });
-                            }
-                            else
-                            {
-                                Parameters?.OnSuccess?.Invoke(ImageInformation, loadingResult);
-                            }
+                            success = true;
 
                             return;
                         }
@@ -402,17 +393,7 @@ namespace FFImageLoading.Work
                     }
                 }
 
-                if (Configuration.ExecuteCallbacksOnUIThread && Parameters?.OnSuccess != null)
-                {
-                    await MainThreadDispatcher.PostAsync(() =>
-                    {
-                        Parameters?.OnSuccess?.Invoke(ImageInformation, loadingResult);
-                    });
-                }
-                else
-                {
-                    Parameters?.OnSuccess?.Invoke(ImageInformation, loadingResult);
-                }
+                success = true;
             }
             catch (Exception ex)
             {
@@ -464,24 +445,28 @@ namespace FFImageLoading.Work
             }
             finally
             {
+                IsCompleted = true;
+
                 using (Parameters)
                 {
                     if (Configuration.ExecuteCallbacksOnUIThread && Parameters?.OnFinish != null)
                     {
                         await MainThreadDispatcher.PostAsync(() =>
                         {
+                            if (success)
+                                Parameters?.OnSuccess?.Invoke(ImageInformation, loadingResult);
                             Parameters?.OnFinish?.Invoke(this);
                         });
                     }
                     else
                     {
+                        if (success)
+                            Parameters?.OnSuccess?.Invoke(ImageInformation, loadingResult);
                         Parameters?.OnFinish?.Invoke(this);
                     }
 
                     ImageService.RemovePendingTask(this);
                 }
-
-                IsCompleted = true;
             }
         }
 
