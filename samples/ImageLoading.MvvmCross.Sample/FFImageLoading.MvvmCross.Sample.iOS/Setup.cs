2c09c1f3c86bfd5c41b40750e0aa8f9111f09ef8
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2017-06-23 12:45:33 -0300

    Added MvvmCross iOS sample project

---
+++ samples/ImageLoading.MvvmCross.Sample/FFImageLoading.MvvmCross.Sample.iOS/Setup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FFImageLoading.MvvmCross.Sample.Core;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using UIKit;
+
+namespace FFImageLoading.MvvmCross.Sample.iOS
+{
+    public class Setup : MvxIosSetup
+    {
+        public Setup(MvxApplicationDelegate applicationDelegate, UIWindow window)
+            : base(applicationDelegate, window)
+        {
+        }
+
+        public Setup(MvxApplicationDelegate applicationDelegate, IMvxIosViewPresenter presenter)
+            : base(applicationDelegate, presenter)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+    }
+}
