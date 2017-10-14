6b979109612f6369098f28be7be5b43617884be7
Daniel Luberda <daniel.luberda@gmail.com>
2017-10-14 15:13:20 +0200

    Added some ImageService unit tests

--- source/Tests/FFImageLoading.Tests/ImageService/ImageServiceBaseTests.cs
+++ source/Tests/FFImageLoading.Tests/ImageService/ImageServiceBaseTests.cs
@@ -13,6 +13,8 @@ namespace FFImageLoading.Tests.ImageServiceTests
             ImageService.EnableMockImageService = true;
         }
 
+        const string RemoteImage = "https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg";
+
         [Fact]
         public void CanInitialize()
         {
@@ -30,15 +32,58 @@ namespace FFImageLoading.Tests.ImageServiceTests
         [Fact]
         public async Task CanDownloadOnly()
         {
-            await ImageService.Instance.LoadUrl("https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg")
+            await ImageService.Instance.LoadUrl(RemoteImage)
                 .DownloadOnlyAsync();
+
+            var diskCacheKey = ImageService.Instance.Config.MD5Helper.MD5(RemoteImage);
+            var cachedDisk = await ImageService.Instance.Config.DiskCache.ExistsAsync(diskCacheKey);
+            Assert.True(cachedDisk);
         }
 
         [Fact]
         public async Task CanPreload()
         {
-            await ImageService.Instance.LoadUrl("https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg")
+            await ImageService.Instance.LoadUrl(RemoteImage)
                 .PreloadAsync();
+
+            var diskCacheKey = ImageService.Instance.Config.MD5Helper.MD5(RemoteImage);
+            var cachedDisk = await ImageService.Instance.Config.DiskCache.ExistsAsync(diskCacheKey);
+            Assert.True(cachedDisk);
+
+            var cachedMemory = Mock.MockImageCache.Instance.Get(RemoteImage);
+            Assert.NotNull(cachedMemory);
+        }
+
+        [Fact]
+        public async Task CanInvalidate()
+        {
+            await ImageService.Instance.LoadUrl(RemoteImage)
+                .PreloadAsync();
+
+            await ImageService.Instance.InvalidateCacheAsync(Cache.CacheType.All);
+
+            var diskCacheKey = ImageService.Instance.Config.MD5Helper.MD5(RemoteImage);
+            var cachedDisk = await ImageService.Instance.Config.DiskCache.ExistsAsync(diskCacheKey);
+            Assert.False(cachedDisk);
+
+            var cachedMemory = Mock.MockImageCache.Instance.Get(RemoteImage);
+            Assert.Null(cachedMemory);
+        }
+
+        [Fact]
+        public async Task CanInvalidateEntry()
+        {
+            await ImageService.Instance.LoadUrl(RemoteImage)
+                .PreloadAsync();
+
+            await ImageService.Instance.InvalidateCacheEntryAsync(RemoteImage, Cache.CacheType.All, true);
+
+            var diskCacheKey = ImageService.Instance.Config.MD5Helper.MD5(RemoteImage);
+            var cachedDisk = await ImageService.Instance.Config.DiskCache.ExistsAsync(diskCacheKey);
+            Assert.False(cachedDisk);
+
+            var cachedMemory = Mock.MockImageCache.Instance.Get(RemoteImage);
+            Assert.Null(cachedMemory);
         }
     }
 }
