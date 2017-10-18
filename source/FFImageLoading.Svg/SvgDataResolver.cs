3758d5d46ef3a1a70ae716eb9315c3d5e4a3857b
Daniel Luberda <daniel.luberda@gmail.com>
2017-10-19 01:16:57 +0200

    Fixes #763 SvgDataResolver not working when using in PCL project

--- source/FFImageLoading.Svg/SvgDataResolver.cs
+++ source/FFImageLoading.Svg/SvgDataResolver.cs
@@ -3,60 +3,58 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using FFImageLoading.Config;
 using FFImageLoading.Work;
 
 namespace FFImageLoading.Svg.Platform
 {
     [Preserve(AllMembers=true)]
-	public class SvgDataResolver : IVectorDataResolver
-	{
-		public static string DoNotReferenceMessage
-		{
-			get
-			{
-				return "You are referencing the Portable version in your App - you need to reference the platform specific version";
-			}
-		}
+    public class SvgDataResolver : IVectorDataResolver
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:FFImageLoading.Svg.Platform.SvgDataResolver"/> class.
+        /// Default SVG size is read from SVG file width / height attributes
+        /// You can override it by specyfing vectorWidth / vectorHeight params
+        /// </summary>
+        /// <param name="vectorWidth">Vector width.</param>
+        /// <param name="vectorHeight">Vector height.</param>
+        /// <param name="useDipUnits">If set to <c>true</c> use dip units.</param>
+        public SvgDataResolver(int vectorWidth = 0, int vectorHeight = 0, bool useDipUnits = true, Dictionary<string, string> replaceStringMap = null)
+        {
+            throw new Exception(DoNotReferenceMessage);
+        }
+
+        public static string DoNotReferenceMessage
+        {
+            get
+            {
+                return "You are referencing the Portable version in your App - you need to reference the platform specific version";
+            }
+        }
+
+        public Configuration Configuration => throw new NotImplementedException(DoNotReferenceMessage);
+
+        public bool UseDipUnits => throw new NotImplementedException(DoNotReferenceMessage);
+
+        public int VectorHeight => throw new NotImplementedException(DoNotReferenceMessage);
+
+        public int VectorWidth => throw new NotImplementedException(DoNotReferenceMessage);
 
         public Dictionary<string, string> ReplaceStringMap
         {
             get
             {
-                throw new Exception(DoNotReferenceMessage);
+                throw new NotImplementedException(DoNotReferenceMessage);
             }
             set
             {
-                throw new Exception(DoNotReferenceMessage);
+                throw new NotImplementedException(DoNotReferenceMessage);
             }
         }
 
-        public bool UseDipUnits
-		{
-			get
-			{
-				throw new Exception(DoNotReferenceMessage);
-			}
-		}
-
-		public int VectorHeight
-		{
-			get
-			{
-				throw new Exception(DoNotReferenceMessage);
-			}
-		}
-
-		public int VectorWidth
-		{
-			get
-			{
-				throw new Exception(DoNotReferenceMessage);
-			}
-		}
-
-		public Task<Tuple<Stream, LoadingResult, ImageInformation>> Resolve(string identifier, TaskParameter parameters, CancellationToken token)
-		{
-			throw new Exception(DoNotReferenceMessage);
-		}
-	}
+        public Task<Tuple<Stream, LoadingResult, ImageInformation>> Resolve(string identifier, TaskParameter parameters, CancellationToken token)
+        {
+            throw new NotImplementedException(DoNotReferenceMessage);
+        }
+    }
 }
