a95987563236a1edb93b1d2a4730d46970910a36
Daniel Luberda <daniel.luberda@gmail.com>
2017-12-30 17:37:26 +0100

    SVG: lineargradient & radialgradient fixes

--- samples/ImageLoading.Forms.Sample/Shared/Pages/BasicPageModel.cs
+++ samples/ImageLoading.Forms.Sample/Shared/Pages/BasicPageModel.cs
@@ -3,14 +3,14 @@ using Xamvvm;
 
 namespace FFImageLoading.Forms.Sample
 {
-	
-	public class BasicPageModel : BasePageModel
-	{
-		public void Reload()
-		{
-			ImageUrl = Helpers.GetRandomImageUrl();
-		}
+    
+    public class BasicPageModel : BasePageModel
+    {
+        public void Reload()
+        {
+            ImageUrl = Helpers.GetRandomImageUrl();
+        }
 
-		public string ImageUrl { get; set; }
-	}
+        public string ImageUrl { get; set; }
+    }
 }
