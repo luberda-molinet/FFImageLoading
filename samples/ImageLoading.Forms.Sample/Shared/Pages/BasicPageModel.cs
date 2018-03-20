a9ab7a1a96dd6f5a2a52e32da0f53c88fe8eed40
Daniel Luberda <daniel.luberda@gmail.com>
2018-03-20 12:06:39 +0100

    Modified basic sample

--- samples/ImageLoading.Forms.Sample/Shared/Pages/BasicPageModel.cs
+++ samples/ImageLoading.Forms.Sample/Shared/Pages/BasicPageModel.cs
@@ -8,7 +8,8 @@ namespace FFImageLoading.Forms.Sample
     {
         public void Reload()
         {
-            ImageUrl = Helpers.GetRandomImageUrl();
+            // ImageUrl = Helpers.GetRandomImageUrl();
+            ImageUrl = @"https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Landscape_3.jpg";
         }
 
         public string ImageUrl { get; set; }
