65c8b2bd768575ef1eaf83045b4909d4ef536c2b
Daniel Luberda <daniel.luberda@gmail.com>
2018-05-28 17:29:11 +0200

    Added Mac sample

---
+++ samples/FFImageLoading.Mac.Sample/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace FFImageLoading.Mac.Sample
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var imageView = new NSImageView
+            {
+                Frame = this.View.Bounds,
+            };
+            this.View.AddSubview(imageView);
+            ImageService.Instance.LoadUrl("https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Landscape_3.jpg").Into(imageView);
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
