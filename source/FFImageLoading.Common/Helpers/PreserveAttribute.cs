b76b272045783027ca3a995823888f18466683ef
Kristoffer Berge <Kristoffer.Berge@knowit.no>
2018-07-09 11:43:01 +0200

    Making annotation visible to other assemblies

--- source/FFImageLoading.Common/Helpers/PreserveAttribute.cs
+++ source/FFImageLoading.Common/Helpers/PreserveAttribute.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("FFImageLoading.Transformations"),
+	InternalsVisibleTo("FFImageLoading.Svg.Forms"),
+	InternalsVisibleTo("FFImageLoading.Transformations"),
+	InternalsVisibleTo("FFImageLoading.Forms.Sample.Droid"),
+	InternalsVisibleTo("FFImageLoading.Forms.Sample.Droid"),
+	InternalsVisibleTo("FFImageLoading.Forms.Sample.Droid"),
+	InternalsVisibleTo("FFImageLoading.Forms.Sample.Droid"),
+	InternalsVisibleTo("FFImageLoading.Forms.Sample.Droid"),
+	InternalsVisibleTo("FFImageLoading.Forms.Sample.Droid"),
+	InternalsVisibleTo("FFImageLoading.Forms.Sample.Droid"),
+	]
 namespace FFImageLoading
 {
-    public sealed class PreserveAttribute : System.Attribute
-    {
-        public bool AllMembers;
-        public bool Conditional;
-    }
+	sealed class PreserveAttribute : System.Attribute
+	{
+		public bool AllMembers;
+		public bool Conditional;
+	}
 }
