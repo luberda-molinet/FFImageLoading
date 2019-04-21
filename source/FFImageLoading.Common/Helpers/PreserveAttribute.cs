496edec602a0b36ef0c710799827b592ec35c8ff
Daniel Luberda <daniel.luberda@gmail.com>
2019-04-21 23:57:40 +0200

    Updated packages

--- source/FFImageLoading.Common/Helpers/PreserveAttribute.cs
+++ source/FFImageLoading.Common/Helpers/PreserveAttribute.cs
@@ -1,9 +1,11 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("FFImageLoading.Transformations"), InternalsVisibleTo("FFImageLoading.Svg.Forms")]
 namespace FFImageLoading
 {
-    sealed class PreserveAttribute : System.Attribute
+	[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate)]
+	public sealed class PreserveAttribute : Attribute
     {
         public bool AllMembers;
         public bool Conditional;
