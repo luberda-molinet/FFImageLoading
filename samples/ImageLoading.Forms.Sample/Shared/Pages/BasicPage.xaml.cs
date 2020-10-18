74892c9a4a9f363f22aa65359ccf29d5b3dd0a3a
Daniel Luberda <daniel.luberda@gmail.com>
2020-10-18 15:14:12 +0200

    Revert "black space issue"
    This reverts commit c878f4a04fef1e4e8cbfe1b7afbd2d108bcd0260.

--- samples/ImageLoading.Forms.Sample/Shared/Pages/BasicPage.xaml.cs
+++ samples/ImageLoading.Forms.Sample/Shared/Pages/BasicPage.xaml.cs
@@ -4,37 +4,14 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using System.Windows.Input;
 using Xamvvm;
-using System.Threading.Tasks;
-using FFImageLoading.Transformations;
 
 namespace FFImageLoading.Forms.Sample
 {
-    public partial class BasicPage : ContentPage, IBasePage<BasicPageModel>
-    {
-        public BasicPage()
-        {
-            InitializeComponent();
-
-            Test();
-        }
-
-        public async Task Test()
-        {
-            ImageService.Instance.InvalidateMemoryCache();
-
-            var stream = ImageService.Instance.LoadUrl("http://loremflickr.com/600/600/nature?filename=simple.jpg").AsJPGStreamAsync(100);
-
-            var task = ImageService.Instance.LoadStream((cancelToken) => stream);
-
-            var taskWithTransforms = task
-                    .WithCache(FFImageLoading.Cache.CacheType.Disk)
-                .Transform(new RotateTransformation(30));
-
-
-            var imageSource = new StreamImageSource();
-            imageSource.Stream = (c) => taskWithTransforms.AsJPGStreamAsync(100);
-
-            testImg.Source = imageSource;
-        }
-    }
+	public partial class BasicPage : ContentPage, IBasePage<BasicPageModel>
+	{
+		public BasicPage()
+		{
+			InitializeComponent();
+		}
+	}
 }
