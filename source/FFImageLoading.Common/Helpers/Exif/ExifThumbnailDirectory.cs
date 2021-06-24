908231665c080822d703c474f5fec0c6a953a939
maonaoda <maonaoda@gmail.com>
2021-06-24 12:56:41 +0900

    fix:fix orientation of Android

---
+++ source/FFImageLoading.Common/Helpers/Exif/ExifThumbnailDirectory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace FFImageLoading.Helpers.Exif
+{
+    internal class ExifThumbnailDirectory : ExifDirectoryBase
+    {
+        /// <summary>The offset to thumbnail image bytes.</summary>
+        public const int TagThumbnailOffset = 0x0201;
+
+        /// <summary>The size of the thumbnail image data in bytes.</summary>
+        public const int TagThumbnailLength = 0x0202;
+
+        public ExifThumbnailDirectory()
+        {
+            SetDescriptor(new ExifThumbnailDescriptor(this));
+        }
+
+        private static readonly Dictionary<int, string> _tagNameMap = new Dictionary<int, string>();
+
+        static ExifThumbnailDirectory()
+        {
+            AddExifTagNames(_tagNameMap);
+        }
+
+        public override string Name => "Exif Thumbnail";
+
+        protected override bool TryGetTagName(int tagType, out string tagName)
+        {
+            return _tagNameMap.TryGetValue(tagType, out tagName);
+        }
+    }
+}
